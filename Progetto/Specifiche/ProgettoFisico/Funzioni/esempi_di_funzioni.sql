df307773555e9409fe6c90df1aae03b92c3f0e6a
Tremors3 <matte.menoz@gmail.com>
2024-06-10 19:49:43 +0200

    Update esempi_di_funzioni.sql

--- Progetto/Specifiche/ProgettoFisico/Funzioni/esempi_di_funzioni.sql
+++ Progetto/Specifiche/ProgettoFisico/Funzioni/esempi_di_funzioni.sql
@@ -6,6 +6,22 @@ CALCOLI E AGGREGAZIONI
     -- Conta il numero di canzoni per una produzione.
    count_songs_in_production(codice_produzione INTEGER) RETURNS INTEGER:
 
+
+CREATE OR REPLACE FUNCTION count_songs_in_production(codice_produzione INTEGER)
+RETURNS INTEGER AS $$
+DECLARE
+    num_songs INTEGER;
+BEGIN
+    -- Conta il numero di canzoni per la produzione specificata
+    SELECT COUNT(*)
+    INTO num_songs
+    FROM canzone
+    WHERE produzione = codice_produzione;
+
+    RETURN num_songs;
+END;
+$$ LANGUAGE plpgsql;
+
 CREATE OR REPLACE FUNCTION calculate_average_song_length(codice_produzione INTEGER)
 RETURNS NUMERIC AS $$
 DECLARE
@@ -36,17 +52,6 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION count_songs_in_production(codice_produzione INTEGER)
-RETURNS INTEGER AS $$
-DECLARE
-    num_songs INTEGER;
-BEGIN
-    -- Conta il numero di canzoni per la produzione specificata
-    SELECT COUNT(*)
-    INTO num_songs
-    FROM canzone
-    WHERE produzione = codice_produzione;
 
-    RETURN num_songs;
-END;
-$$ LANGUAGE plpgsql;
+
+
